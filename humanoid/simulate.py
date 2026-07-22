1c1ced0d066805ac8f1c66ecd5948492e408ace1
Palak Singhi <singhip600@gmail.com>
2026-07-22 16:46:58 +0530

    adding the humanoid env,simulation and xml files

---
+++ humanoid/simulate.py
@@ -0,0 +1,9 @@
+import mujoco
+import mujoco.viewer
+model=mujoco.MjModel.from_xml_path("humanoid.xml")
+data=mujoco.MjData(model)
+
+with mujoco.viewer.launch_passive(model,data) as viewer:
+    while viewer.is_running():
+        mujoco.mj_step(model,data)
+        viewer.sync()
